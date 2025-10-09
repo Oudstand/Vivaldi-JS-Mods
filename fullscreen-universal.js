d86e857bfb26f78909954ef5090309898a4fde5d
makastn <mark.kastner@wilken.de>
2025-10-09 15:39:11 +0200

    fix omni dropdown not visible behind bookmarks

--- fullscreen-universal.js
+++ fullscreen-universal.js
@@ -353,7 +353,7 @@
                     position: absolute;
                     left: 0;
                     right: 0;
-                    z-index: 7;
+                    z-index: 5;
                 }
     
                 #header .vivaldi {
